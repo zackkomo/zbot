578a3345ed793c93c6fa288018b5c219f605e759
Zack <zack.komo98@gmail.com>
2020-05-01 13:29:38 -0500

    fixed help

--- commands/help.js
+++ commands/help.js
@@ -3,26 +3,36 @@ const fs = require("fs");
 
 
 module.exports.run = async (bot, message, args) => {
-    if(args.length <1) return message.channel.send("Ask about a command by sending !help [command name]. For a list of commands type !help commands");
-    
-    fs.readdir(__dirname, (err,files) =>{
-        if (err) console.error(err);
-        
-        
-        let jsfiles = files.filter(f => f.split(".").pop() === "js");
-        let cmdnames = "Available commands: \n";
+    if (args.length < 1) return message.channel.send("Ask about a command by sending !help [command name]. For a list of commands type !help commands");
 
-        jsfiles.forEach((f,i) => {
-        let props = require(`./${f}`);
-            if(props.help.name === args[0]){
-                message.channel.send("!"+args[0] + " " + props.help.description);
+    if (args.length != 0) {
+        fs.readdir(__dirname, (err, files) => {
+            if (err) console.error(err);
+
+            let jsfiles = [];
+            let Commandfiles = files.filter(f => {
+                if (f.endsWith("js") && ! f.includes("Utils")){
+                    f = f.split(".");
+                    jsfiles.push(f[0]);
+                }
+            });
+
+            console.log(jsfiles);
+            let cmdnames = "```Available commands: \n";
+
+            jsfiles.forEach((f, i) => {
+                let props = require(`./${f}`);
+                if (props.help.name === args[0]) {
+                    message.channel.send("```" + " !" + args[0] + " " + props.help.description + "```");
+                }
+                cmdnames += props.help.name + "\n";
+            })
+            cmdnames += "```";
+            if (args[0] === "commands"){
+            message.channel.send(cmdnames);
             }
-            cmdnames += props.help.name + "\n";
         })
-
-        message.channel.send(cmdnames);
-        
-    })
+    }
 }
 
 module.exports.help = {
