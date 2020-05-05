88be59c34c1917fe48776a3ec6375e2442a93fb7
Zack <zack.komo98@gmail.com>
2020-05-04 23:35:20 -0500

    testing pushing

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("<@" + message.author + ">" + " Pong!");
+    message.channel.send("<@" + message.author + ">" + " Pong! test");
 }
 
 module.exports.help = {
