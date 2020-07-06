2c8b31d385e9cee994ca2fe26f11fcf285dea49f
Zack Komodromos <zkomodromos@ct.local>
2020-07-06 15:20:05 -0500

    making category for channel creating command

--- commands/channel.js
+++ commands/channel.js
@@ -26,46 +26,71 @@ module.exports.run = async (bot, message, args) => {
     let title = messageArr.shift();
 
 
+    server.channels.create("Personal Text Channels", {type : 'category'}).
+    
+    // let category = server.channels.cache.find(c => c.name == "Personal Text Channels" && c.type == "category"),
+    //     channel = server.channels.cache.find(c => c.name == "general" && c.type == "text");
 
-    let permissions = [{
-        id: message.guild.id,
-        deny: ['VIEW_CHANNEL'],
-    },
-    {
-        id: message.guild.member(bot.user).id,
-        allow: ['VIEW_CHANNEL'],
-    },
-    {
-        id: message.author.id,
-        allow: ['VIEW_CHANNEL'],
-    }
-    ];
+    // if (!!category){
+    //     console.log("In")
+    //     await server.channels.create("Personal Text Channels", {type : "category"})
+    // }
 
-    let re = /\s|<|>|!|@/g;
+    // if (category && channel) channel.setParent(category.id);
+    // else console.error(`One of the channels is missing:\nCategory: ${!!category}\nChannel: ${!!channel}`);
+    
+    // // find(async c => {
+    //     console.log("in for " + c)
+    //     return (c.name === "Personal Text Channels" && c.type === "category");
+    // });
+    console.log(category);
 
-    for (let i = 0; i < messageArr.length; i++) {
-        if (!message.guild.member(messageArr[i].replace(re, ""))) {
-            message.author.send("One of the users provided does not exist, try again");
-            return message.delete();
-        }
 
-        permissions.push({
-            id: messageArr[i].replace(re, ""),
-            allow: ['VIEW_CHANNEL']
-        })
 
-    }
-    let options = {
-        type: "text",
-        permissionOverwrites: permissions
-    }
+    // if (category && channel) channel.setParent(category.id);
+    // else console.error(`One of the channels is missing:\nCategory: ${!!category}\nChannel: ${!!channel}`);
+    // console.log(server.channels.cache)
 
-    let newChannel = server.channels.create(title, options).catch(err => {
-        console.log(err);
-        message.author.send(err.message);
-        return message.delete();
-    })
-    message.delete();
+
+    // let permissions = [{
+    //     id: message.guild.id,
+    //     deny: ['VIEW_CHANNEL'],
+    // },
+    // {
+    //     id: message.guild.member(bot.user).id,
+    //     allow: ['VIEW_CHANNEL'],
+    // },
+    // {
+    //     id: message.author.id,
+    //     allow: ['VIEW_CHANNEL'],
+    // }
+    // ];
+
+    // let re = /\s|<|>|!|@/g;
+
+    // for (let i = 0; i < messageArr.length; i++) {
+    //     if (!message.guild.member(messageArr[i].replace(re, ""))) {
+    //         message.author.send("One of the users provided does not exist, try again");
+    //         return message.delete();
+    //     }
+
+    //     permissions.push({
+    //         id: messageArr[i].replace(re, ""),
+    //         allow: ['VIEW_CHANNEL']
+    //     })
+
+    // }
+    // let options = {
+    //     type: "type",
+    //     permissionOverwrites: permissions
+    // }
+
+    // let newChannel = server.channels.create(title, options).catch(err => {
+    //     console.log(err);
+    //     message.author.send(err.message);
+    //     return message.delete();
+    // })
+    //message.delete();
 }
 
 module.exports.help = {
