754a43d5b27d92f6be7d4b8bf01518c8dcb2da73
Zack <zack.komo98@gmail.com>
2020-07-07 17:19:12 -0500

    commented code

--- commands/pollCheck.js
+++ commands/pollCheck.js
@@ -2,11 +2,15 @@ const Discord = module.require("discord.js");
 const store = require(__dirname + "/pollUtils.js");
 
 module.exports.run = async (bot, message, args) => {
-    store.check(args[0], message);   
-    
+    //use pollUtils check to access pollList.json and check on poll
+    store.check(args[0], message);
 }
 
 module.exports.help = {
     name: "pollCheck",
-    description: 'Description: You can check on polls. If no arguements are added (use 1) then a list of available polls for the chanell are posted. If you add a number after the command (use 2), then it will repost that specific poll and voting on the new post will be valid. Voting on the older post will no longer work.\nUse 1: !pollcheck\nUse 2: !pollCheck [number]'
+    description: 'Description: You can check on polls. If no arguements are added'
+    + '(use 1) then a list of available polls for the channel are posted. If you add'
+    + ' a number after the command (use 2), then it will repost that specific poll'
+    + ' and voting on the new post will be valid while voting on the older post will no'
+    + ' longer work.\nUse 1: !pollcheck\nUse 2: !pollCheck [number]'
 }
