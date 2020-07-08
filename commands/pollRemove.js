f98c3d11cc3d3c820749df90ecbc5007324a0c25
Zack <zack.komo98@gmail.com>
2020-07-07 21:52:31 -0500

    commented code

--- commands/pollRemove.js
+++ commands/pollRemove.js
@@ -2,11 +2,14 @@ const Discord = module.require("discord.js");
 const store = require(__dirname + "/pollUtils.js");
 
 module.exports.run = async (bot, message, args) => {
+    //use pollUtils remove to delete poll from memory
     store.remove(args[0], message);   
-    
 }
 
 module.exports.help = {
     name: "pollRemove",
-    description: 'Description: Removes a poll based on the poll number following the command. When you ender the command, it will show you the poll you are trying to remove and prompt you for a confirmation (y for yes and n for no). To remind yourself of the poll number, use !pollCheck.\nUse : !pollRemove [poll number]'
+    description: 'Description: Removes a poll based on the poll number following the command.'
+    + 'When you enter the command, it will show you the poll you are trying to remove and prompt'
+    + ' you for a confirmation (y for yes and n for no). To remind yourself of the poll number,'
+    + ' use !pollCheck.\nUse : !pollRemove [poll number]'
 }
